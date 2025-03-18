12bad9c0a27cd9e44aaa53ce3e81dd508c113b4a
fredimania <fredimania@gmail.com>
2025-03-18 08:13:02 +0100

    CAMBIO HOME

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>POKEMON</h1>
+      <h1>POKEMON GENERADO POR JENKINS</h1>
       <p>Pokemon por generación trabajo de IAW</p>
       <Outlet />
     </div>
